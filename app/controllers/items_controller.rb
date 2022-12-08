b0ba66e950c9931f47e7f0abf20a86b65edebf81
maythetsoe <maythetsoe1994@gmail.com>
2022-12-08 22:48:28 +0900

    商品情報編集機能　修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_tweet, only: [:edit, :show, :update]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     if user_signed_in? && current_user.id == @item.user_id 
       redirect_to item_path  unless current_user.id == @item.user_id
     else
@@ -27,7 +29,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
   if @item.update(item_params)
     redirect_to item_path
   else
@@ -36,7 +37,6 @@ class ItemsController < ApplicationController
 end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
@@ -44,4 +44,15 @@ end
   def item_params
     params.require(:item).permit(:name, :description, :detail_category_id, :detail_commodity_condition_id, :delivery_burden_id, :region_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_tweet
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
+
