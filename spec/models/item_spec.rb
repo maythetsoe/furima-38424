c9cad33d21038796da4148c5f853e8111b02dc32
maythetsoe <maythetsoe1994@gmail.com>
2022-12-08 14:55:20 +0900

    商品出品機能　修正2回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include "Description can't be blank"
   end
   it 'カテゴリーに「---」が選択されている場合は出品できない' do
-    @item.detail_category_id = '---'
+    @item.detail_category_id = '1'
   @item.valid?
   expect(@item.errors.full_messages).to include "Detail category can't be blank"
   end
    it '商品の状態に「---」が選択されている場合は出品できない' do
-   @item.detail_commodity_condition_id = '---'
+   @item.detail_commodity_condition_id = '1'
    @item.valid?
     expect(@item.errors.full_messages).to include "Detail commodity condition can't be blank"
    end
    it '配送料の負担に「---」が選択されている場合は出品できない' do
-  @item.delivery_burden_id = '---'
+  @item.delivery_burden_id = '1'
   @item.valid?
    expect(@item.errors.full_messages).to include "Delivery burden can't be blank"
    end
    it '発送元の地域に「---」が選択されている場合は出品できない' do
-   @item.region_id = '---'
+   @item.region_id = '1'
    @item.valid?
   expect(@item.errors.full_messages).to include "Region can't be blank"
   end
    it '発送までの日数に「---」が選択されている場合は出品できない' do
-   @item.days_to_ship_id = '---'
+   @item.days_to_ship_id = '1'
    @item.valid?
    expect(@item.errors.full_messages).to include "Days to ship can't be blank"
    end
