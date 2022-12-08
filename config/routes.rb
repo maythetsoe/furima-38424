db98f5fb0dbb3e73989a7976d81ecc67dc838f84
maythetsoe <maythetsoe1994@gmail.com>
2022-12-08 19:29:27 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index', to: 'users#index'
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create]
-  resources :articles
+  resources :items, only: [:index, :new, :create, :show]
 end
